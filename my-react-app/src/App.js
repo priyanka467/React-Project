6d2dd773846e8a77c61cbc5a0bc8d1ad1b62c35b
Priyanka <73979941+priyanka467@users.noreply.github.com>
2024-10-20 21:07:56 +0000

    2nd commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from '../src/assets/images/LOGO.png';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//         TECH कला
+//         </p>
+        
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useEffect, useState } from 'react';
+import logo from '../src/assets/images/LOGO.png';
+import '../src/CSS/App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+
+    return () => clearTimeout(timer); // Cleanup on unmount
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="App-header">
+      <p>Loading...</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        TECH कला
+      </p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TECH कला
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
