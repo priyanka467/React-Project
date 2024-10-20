6b89866a15f2a16421d631c1e2dcef783e3cf1f0
Priyanka <73979941+priyanka467@users.noreply.github.com>
2024-10-20 21:44:41 +0000

    4th commit

--- my-react-app/src/UI_Pages/SplashScreen.js
+++ my-react-app/src/UI_Pages/SplashScreen.js
@@ -1,57 +1,36 @@
-// import logo from '../src/assets/images/LOGO.png';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//         TECH कला
-//         </p>
-        
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import logo from '../assets/images/LOGO.png';
 import '../CSS/App.css';
 
 function SplashScreen() {
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Simulate loading time
     const timer = setTimeout(() => {
       setLoading(false);
+      navigate('/registration'); // Navigate to registration after loading
     }, 2000); // 2 seconds
 
     return () => clearTimeout(timer); // Cleanup on unmount
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="App-header">
-      <p>Loading...</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        TECH कला
-      </p>
-      </div>
-    );
-  }
+  }, [navigate]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TECH कला
-        </p>
-      </header>
+    <div className="App-header">
+      {loading ? (
+        <>
+          <p>Loading...</p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>TECH कला</p>
+        </>
+      ) : (
+        <header className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>TECH कला</p>
+        </header>
+      )}
     </div>
   );
 }
